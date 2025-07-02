4192a2608eadd0c1d40b745a908a4b1901741661
MD Emon Talukdar <emon14291@gmail.com>
2025-07-02 22:52:20 +0600

    signup form ready

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import AuthForm from '@/app/components/AuthForm'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+     <AuthForm type="sign-in" />
+    </div>
+  )
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function page() {
-  return (
-    <div className='font-bold text-4xl text-center mt-44 bg-amber-200 py-4'>
-      hello lexi mock ai
-    </div>
-  )
-}
